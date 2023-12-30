8cc968d7d6f5797b0e7379e1028bba12674b8573
Juan Acevedo <jmamartinez18@gmail.com>
2023-12-29 20:20:20 -0700

    corrected inquire version and got questions to console log thoroughly

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+const inquirer = require("inquirer");
+// const fs = require('fs');
 
+const questions = [
+    {
+        type: "input",
+        name: "logoText",
+        message: "Enter characters for logo (up to 3 max):",
+    },
+    {
+        type: "input", 
+        name: "charColor",
+        message: "Enter color or HEX code desired for the text:",
+    },
+    {
+        type: "list",
+        name: "shapeSelected",
+        Message: "Select a shape for your logo:",
+        choices: ["Square", "Triangle", "Circle"],
+    },
+    {
+        type: "input",
+        name: "shapeColor",
+        message: "Enter the color or HEX code desired for the color of the shape:",
+    },
+];
+
+function init(){
+    inquirer.prompt(questions).then((Responses) => {
+        console.log("Generated Logo SVG");
+    });
+}
+
+init()
