4b40744976a4d004937f404e65e71acae0a2c917
Juan Acevedo <jmamartinez18@gmail.com>
2024-01-03 21:04:11 -0700

    added svg class and corrected test to run required triangle code

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const {Square, Triangle, Circle} = require("./lib/shapes")
+const svg = require("./lib/svg")
 
-const questions = [
-    {
-        type: "input",
-        name: "logoText",
-        message: "Enter characters for logo (up to 3 max):",
-    },
-    {
-        type: "input", 
-        name: "charColor",
-        message: "Enter color or HEX code desired for the text:",
-    },
-    {
-        type: "list",
-        name: "shapeSelected",
-        Message: "Select a shape for your logo:",
-        choices: ["Square", "Triangle", "Circle"],
-    },
-    {
-        type: "input",
-        name: "shapeColor",
-        message: "Enter the color or HEX code desired for the color of the shape:",
-    },
-];
 
-function init(){
-    inquirer.prompt(questions).then((Responses) => {
-        console.log("Generated Logo SVG");
-    });
-}
+function questions() {
+    inquirer.prompt([
+            {
+                type: "input",
+                name: "text",
+                message: "Enter characters for logo (up to 3 max):",
+            },
+            {
+                type: "input", 
+                name: "textColor",
+                message: "Enter color or HEX code desired for the text:",
+            },
+            {
+                type: "list",
+                name: "shape",
+                Message: "Select a shape for your logo:",
+                choices: ["Square", "Triangle", "Circle"],
+            },
+            {
+                type: "input",
+                name: "shapeColor",
+                message: "Enter the color or HEX code desired for the color of the shape:",
+            },
+        ])        
+};
 
-init()
+questions();
