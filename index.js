9df29b764894bb97e1b10c0bc29d2d2dd595834e
Juan Acevedo <jmamartinez18@gmail.com>
2024-01-03 22:27:28 -0700

    got svg to render to the examples folder and corrected text placement on circle

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const inquirer = require("inquirer");
-const fs = require("fs");
+const fs = require("fs/promises");
 const {Square, Triangle, Circle} = require("./lib/shapes")
-const svg = require("./lib/svg")
+const SVG = require("./lib/svg");
 
 
-function questions() {
-    inquirer.prompt([
+const questions = () => {
+    inquirer
+        .prompt([
             {
                 type: "input",
                 name: "text",
@@ -27,7 +28,29 @@ function questions() {
                 name: "shapeColor",
                 message: "Enter the color or HEX code desired for the color of the shape:",
             },
-        ])        
+        ])
+        .then((res) => {
+            let renderedShape;
+
+            if (res.shape === "Square") {
+                renderedShape = new Square();
+            }
+            if (res.shape === "Triangle") {
+                renderedShape = new Triangle();
+            }
+            if (res.shape === "Circle") {
+                renderedShape = new Circle();
+            }
+            renderedShape.setColor(res.shapeColor)
+
+            const svg = new SVG();
+            svg.setText(res.text, res.textColor);
+            svg.setShape(renderedShape);
+            return fs.writeFile("./examples/logo.svg", svg.render());
+        }).then(() => {
+            console.log("Generated logo.svg!");
+        })
+       
 };
 
 questions();
