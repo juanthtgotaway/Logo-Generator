71f496519ea337e3fb8b716efb3e327646822626
Juan Acevedo <jmamartinez18@gmail.com>
2024-01-04 22:46:45 -0700

    added comments to all files

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+//This code imports all necessary modules and packages to run application 
 const inquirer = require("inquirer");
 const fs = require("fs/promises");
+//imports the classes 
 const {Square, Triangle, Circle} = require("./lib/shapes")
 const SVG = require("./lib/svg");
 
-
+//Questions asked using inquirer prompt that wil be used to later help render the logo based off of user input
 const questions = () => {
     inquirer
         .prompt([
@@ -30,8 +32,10 @@ const questions = () => {
             },
         ])
         .then((res) => {
+            //renderedShape is used to store information for the shape
             let renderedShape;
 
+            //creates new shape based on information user input above
             if (res.shape === "Square") {
                 renderedShape = new Square();
             }
@@ -43,14 +47,16 @@ const questions = () => {
             }
             renderedShape.setColor(res.shapeColor)
 
+            //Creates svg file and saves it inside of the examples folder as logo.svg
             const svg = new SVG();
             svg.setText(res.text, res.textColor);
             svg.setShape(renderedShape);
             return fs.writeFile("./examples/logo.svg", svg.render());
         }).then(() => {
+            //once all is input then generated logo.svg is logged into terminal to show that shape was rendered successfully 
             console.log("Generated logo.svg!");
         })
        
 };
-
+//calls the questions to initiate the whole thing
 questions();
